d0bf5878d1687d38d7d5fae7efc5935bb7fc51e1
Rizki Maulana <rrizki402@gmail.com>
2024-02-17 16:21:34 +0700

    Deploy Admin Saving 3

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import Home from './component/Home';
+import About from './component/About';
+import ContactForm from './component/ContactForm';
+// import SignUp from './component/SignUp';
+import SignUp2 from "./component/SingUp2";
+import Home2 from './component/Home2';
+import PlanMore2 from './platform/PlanMore2';
+import PlanMore3 from './platform/PlanMore3';
+import PlanMore4 from './platform/PlanMore4';
+import Navbar from './platform/Navbar';
+import PlanMoreContent from './platform/PlanMoreContent';
+import TransactionInvoice from './component/TransactionInvoice';
+import PaymentForm from './component/PaymentForm';
+import ContactForm2 from './component/ContactForm2';
+import About2 from './component/About2';
+import SignUp3 from './component/SignUp3';
+import PaymentForm2 from './component/PaymentForm2';
+
+function App() {
+  const isLoggedIn = true; // Set this based on your authentication logic
+  return (
+    <div className="App">
+      {/* <Navbar/> */}
+      <Routes>
+        {/* <Route path='/' element={<Home/>}/> */}
+        <Route path='/' element={<SignUp2/>}/>
+        {/* <Route path='/' element={<Home2/>}/> */}
+        <Route path='/home' element={<Home2/>}/>
+        <Route path='/home/:approval' element={<Home2/>} />
+        <Route path='/about' element={<About/>}/>
+        {/* <Route path='/contact' element={<ContactForm/>}/> */}
+        <Route path='/about2' element={<About2/>}/>
+        {/* <Route path='/contact2' element={<ContactForm2/>}/> */}
+        <Route path='/signup' element={<SignUp2/>}/>
+        <Route path='/planing' element={<PlanMore2/>}/>
+        <Route path='/planing2' element={<PlanMore3/>}/>
+        <Route path='/planing3' element={<PlanMore4/>}/>
+        <Route path='/transaksi' element={<TransactionInvoice/>}/>
+        <Route path='/paymentForm' element={<SignUp3/>}/>
+        <Route path='/paymentForm2' element={<SignUp3/>}/>
+        {!isLoggedIn && <Route path="/signup" element={<SignUp2 />} />}
+      </Routes>
+      {/* <Routes>
+        <Route path='/' element={<Home2/>}/>
+        <Route path='/home' element={<Home2/>}/>
+      </Routes> */}
+    </div>
+  );
+}
+
+export default App;
